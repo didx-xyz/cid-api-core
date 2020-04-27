f2af37edd2d669f4e32f9e0048d3b97443a0e17b
Dirk <dirk@thedelta.io>
2020-04-27 21:30:25 +0200

    org fix

--- V1/DTOs/Organisation/OrganisationDTO.cs
+++ V1/DTOs/Organisation/OrganisationDTO.cs
@@ -5,7 +5,7 @@ namespace CoviIDApiCore.V1.DTOs.Organisation
     public class OrganisationDTO
     {
         public string Id { get; set; }
-        public int Balance { get; set; }
+        public int? Balance { get; set; }
         public int Total { get; set; }
 
         public OrganisationDTO()
@@ -16,7 +16,7 @@ namespace CoviIDApiCore.V1.DTOs.Organisation
         public OrganisationDTO(Models.Database.Organisation organisation, OrganisationCounter orgCounter, int total)
         {
             Id = organisation.Id.ToString();
-            Balance = orgCounter.Balance;
+            Balance = orgCounter?.Balance ?? 0;
             Total = total;
         }
     }
