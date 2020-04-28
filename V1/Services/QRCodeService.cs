a00fbd15257087430df4d507720e641787b71ea0
Dirk <dirk@thedelta.io>
2020-04-28 08:46:14 +0200

    clean up

--- V1/Services/QRCodeService.cs
+++ V1/Services/QRCodeService.cs
@@ -4,14 +4,24 @@ using CoviIDApiCore.Exceptions;
 using CoviIDApiCore.V1.Constants;
 using CoviIDApiCore.V1.Interfaces.Services;
 using IronBarCode;
+using Microsoft.Extensions.Configuration;
 
 namespace CoviIDApiCore.V1.Services
 {
     public class QRCodeService : IQRCodeService
     {
+        private readonly IConfiguration _configuration;
+
+        public QRCodeService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public string GenerateQRCode(string id)
         {
-            var qrCode = QRCodeWriter.CreateQrCode(id, 400, QRCodeWriter.QrErrorCorrectionLevel.Medium);
+            var baseUrl = _configuration.GetValue<string>("CoviIDBaseUrl");
+
+            var qrCode = QRCodeWriter.CreateQrCode($"{baseUrl}{UrlConstants.PartialRoutes[UrlConstants.Routes.Organisation]}/{id}", 400, QRCodeWriter.QrErrorCorrectionLevel.Medium);
 
             if(!qrCode.Verify())
                 throw new QRException(Messages.QR_Failed);
