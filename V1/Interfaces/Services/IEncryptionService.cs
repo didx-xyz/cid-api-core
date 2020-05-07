cbc6c2bbc87084a2412c6c4e5568255950701165
Andrew van Rooyen <wraith.andrew@gmail.com>
2020-05-07 09:33:36 +0200

    Include encryption/decryption for TestResults

--- V1/Interfaces/Services/IEncryptionService.cs
+++ V1/Interfaces/Services/IEncryptionService.cs
@@ -1,14 +1,19 @@
 using System.Threading.Tasks;
+// TODO: All references to WalletContract below will change to reference the "new" version
 using CoviIDApiCore.V1.DTOs.Wallet;
+// TODO: All references to CovidTestCredentialParameters below will change to reference the "new" version
+using CoviIDApiCore.V1.DTOs.Credentials;
 
 namespace CoviIDApiCore.V1.Interfaces.Services
 {
     public interface IEncryptionService
     {
-        // TODO: All references to WalletContract below will change to reference the "new" version
-        Task<string> GenerateSecretKey();
-        Task<WalletContract> EncryptWallet(WalletContract plainTextWallet, string secretKey);
-        // TODO: secretKey here should maybe be encryptedSecretKey
-        Task<WalletContract> DecryptWallet(WalletContract encryptedWallet, string secretKey);
+        Task<string> GenerateEncryptedSecretKey();
+
+        Task<WalletContract> EncryptWalletDetails(WalletContract plainTextWallet, string encryptedSecretKey);
+        Task<WalletContract> DecryptWalletDetails(WalletContract encryptedWallet, string encryptedSecretKey);
+
+        Task<CovidTestCredentialParameters> EncryptTestResults(CovidTestCredentialParameters plainTextResults, string encryptedSecretKey);
+        Task<CovidTestCredentialParameters> DecryptTestResults(CovidTestCredentialParameters encryptedResults, string encryptedSecretKey);
     }
 }
--- V1/Services/EncryptionService.cs
+++ V1/Services/EncryptionService.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Threading.Tasks;
+// TODO: All references to WalletContract below will change to reference the "new" version
 using CoviIDApiCore.V1.DTOs.Wallet;
+// TODO: All references to CovidTestCredentialParameters below will change to reference the "new" version
+using CoviIDApiCore.V1.DTOs.Credentials;
 using CoviIDApiCore.V1.Interfaces.Services;
 using Microsoft.Extensions.Configuration;
 
@@ -16,24 +19,38 @@ namespace CoviIDApiCore.V1.Services
             serverKey = configuration.GetValue<string>("ServerKey");
         }
 
-        public Task<string> GenerateSecretKey ()
+        public Task<string> GenerateEncryptedSecretKey ()
         {
             // TODO
-            return Task.FromResult<string>("totally_secret_key");
+            return Task.FromResult<string>("totally_encrypted_secret_key");
         }
 
-        public Task<WalletContract> EncryptWallet(WalletContract plainTextWallet, string secretKey)
+        public Task<WalletContract> EncryptWalletDetails(WalletContract plainTextWallet, string encryptedSecretKey)
         {
             // TODO
             var encryptedWallet = new WalletContract();
             return Task.FromResult<WalletContract>(encryptedWallet);
         }
 
-        public Task<WalletContract> DecryptWallet(WalletContract encryptedWallet, string secretKey)
+        public Task<WalletContract> DecryptWalletDetails(WalletContract encryptedWallet, string encryptedSecretKey)
         {
             // TODO
             var plainTextWallet = new WalletContract();
             return Task.FromResult<WalletContract>(plainTextWallet);
+        }
+
+        public Task<CovidTestCredentialParameters> EncryptTestResults(CovidTestCredentialParameters plainTextResults, string encryptedSecretKey)
+        {
+            // TODO
+            var encryptedResults = new CovidTestCredentialParameters();
+            return Task.FromResult<CovidTestCredentialParameters>(encryptedResults);
+        }
+
+        public Task<CovidTestCredentialParameters> DecryptTestResults(CovidTestCredentialParameters encryptedResults, string encryptedSecretKey)
+        {
+            // TODO
+            var plainTextResults = new CovidTestCredentialParameters();
+            return Task.FromResult<CovidTestCredentialParameters>(plainTextResults);
         }
     }
 }
