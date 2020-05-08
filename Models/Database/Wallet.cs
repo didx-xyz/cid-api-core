11e593649c5fd8998b19b60df873e59d9f1f9bcf
Roleen20 <roleen@thedelta.io>
2020-05-08 08:55:11 +0200

    Updated attribute

--- Models/Database/Wallet.cs
+++ Models/Database/Wallet.cs
@@ -6,9 +6,9 @@ namespace CoviIDApiCore.Models.Database
 {
     public class Wallet : BaseModel<Guid>
     {
-        [Encrypted]
+        [Encrypted(true)]
         public string MobileNumber { get; set; }
-        [Encrypted]
+        [Encrypted(true)]
         public string MobileNumberReference { get; set; }
         public DateTime MobileNumberVerifiedAt { get; set; }
         public DateTime CreatedAt { get; set; }
